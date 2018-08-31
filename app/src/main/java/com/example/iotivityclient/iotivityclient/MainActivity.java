207a168a3a2f104ee910c860b0eb06c09146467d
youngwook <rongxux@gmail.com>
2018-08-31 13:29:44 +0900

    commands for codes

--- app/src/main/java/com/example/iotivityclient/iotivityclient/MainActivity.java
+++ app/src/main/java/com/example/iotivityclient/iotivityclient/MainActivity.java
@@ -57,15 +57,46 @@ public class MainActivity extends AppCompatActivity implements OcPlatform.OnReso
             }
         });
     }
+
     public void startIoTivityClient(){
+
+        /** PlatformConfig class
+         * @param context          app context
+         * @param serviceType      indicate IN_PROC or OUT_OF_PROC
+         * @param modeType         indicate whether we want to do server, client or both
+         * @param ipAddress        ip address of server
+         *                         if you specify 0.0.0.0 : it listens on any interface
+         * @param port             port of server
+         *                         if you specify 0 : next available random port is used
+         *                         if you specify 5683 : client discovery can work even if
+         *                         they don't specify port
+         * @param qualityOfService quality of service
+         */
         Log.d(TAG, "initialize IoTivity Client");
+        //configuration of the Iotivity platform using PlatformConfig class
         PlatformConfig platformConfig = new PlatformConfig(this, ServiceType.IN_PROC, ModeType.CLIENT,
                 "0.0.0.0", 0, QualityOfService.LOW);
         OcPlatform.Configure(platformConfig);
 
+        /** OcPlatform.findResource(...)
+         * API for Service and Resource Discovery.
+         * <p>
+         * Note: This API is for client side only.
+         * </p>
+         *
+         * @param host                    Host IP Address of a service to direct resource discovery query.
+         *                                If empty, performs multicast resource discovery query
+         * @param resourceUri             name of the resource. If null or empty, performs search for all
+         *                                resource names
+         * @param connectivityTypeSet     Set of types of connectivity. Example: IP
+         * @param onResourceFoundListener Handles events, success states and failure states.
+         * @param qualityOfService        the quality of communication
+         * @throws OcException if failure
+         */
         Log.d(TAG, "find resource use transport ip");
         String uri = "coap://"+ IP +OcPlatform.WELL_KNOWN_QUERY + "?rt=core.temperature";
         try{
+            //discover resources. when discovered callback onResourceFoundListener which we implemented
             OcPlatform.findResource("", uri, EnumSet.of(OcConnectivityType.CT_DEFAULT), this);
         } catch (OcException e){
             Log.d(TAG,"error for resource find :" + e.toString());
@@ -74,6 +105,7 @@ public class MainActivity extends AppCompatActivity implements OcPlatform.OnReso
 
     @Override
     public synchronized void onResourceFound(OcResource ocResource) {
+        //OcPlatform.OnResourceFoundListener as a callback when resources have been discovered & return OcResource
         if (null == ocResource){
             Log.d(TAG, "Found resource is invalid");
             return;
@@ -82,6 +114,7 @@ public class MainActivity extends AppCompatActivity implements OcPlatform.OnReso
         if(ocResource.getUri().equals(resourceName)){
             HashMap<String,String> queryResults = new HashMap<>();
             try{
+                //return map of values to query & call back onGetListener
                 ocResource.get(queryResults, this);
             }catch (OcException e){
                 Log.d(TAG, "get resource data error :" + e.toString());
@@ -99,8 +132,10 @@ public class MainActivity extends AppCompatActivity implements OcPlatform.OnReso
 
     @Override
     public synchronized void onGetCompleted(List<OcHeaderOption> list, OcRepresentation ocRepresentation) {
+        //OcResource.OnGetListener as a callback & ocRepresentation hold map of values
         Log.d(TAG, "get temperature");
         try{
+            //query needed value
             String temp = ocRepresentation.getValue("temperature");
             msg(temp);
             Log.d(TAG, "temperature is: " + temp);
